c279ce59a2f317acfda5bc1196a0b98dbf9dc75a
Akkas Singh <akkassingh@Akkass-MacBook-Air.local>
2019-08-17 15:12:15 +0530

    registering user

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const flash = require('connect-flash');
 const session = require('express-session');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const passport = require('passport');
 
 const app = express();
 
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+
+//Load User Model
+require('../models/User')
+const User = mongoose.model('users')
 
 // User Login Route
 router.get('/login', (req, res) => {
@@ -16,15 +21,19 @@ router.get('/register', (req, res) => {
 router.post('/register', (req, res) => {
   let errors = [];
 
-  if(req.body.password != req.body.password2){
-    errors.push({text:'Passwords do not match'});
+  if (req.body.password != req.body.password2) {
+    errors.push({
+      text: 'Passwords do not match'
+    });
   }
 
-  if(req.body.password.length < 4){
-    errors.push({text:'Password must be at least 4 characters'});
+  if (req.body.password.length < 4) {
+    errors.push({
+      text: 'Password must be at least 4 characters'
+    });
   }
 
-  if(errors.length > 0){
+  if (errors.length > 0) {
     res.render('users/register', {
       errors: errors,
       name: req.body.name,
@@ -33,7 +42,33 @@ router.post('/register', (req, res) => {
       password2: req.body.password2
     });
   } else {
-    res.send('passed');
+    User.findOne({email: req.body.email})
+        .then(user => {
+          if(user){
+            req.flash('error_msg', 'Email already registered')
+            res.redirect('/users/login')
+          }else{
+            const newUser = new User({
+              name: req.body.name,
+              email: req.body.email,
+              password: req.body.password
+            })
+            bcrypt.genSalt(10, (err, salt) => {
+              bcrypt.hash(newUser.password, salt, (err, hash) => {
+                if (err) throw err;
+                newUser.password = hash;
+                newUser.save()
+                  .then(user => {
+                    req.flash('success_msg', 'Registered Successfully')
+                    res.redirect('/users/login')
+                  })
+                  .catch(err => {
+                    return;
+                  })
+              })
+            })  
+          }
+        })
   }
 });
 
